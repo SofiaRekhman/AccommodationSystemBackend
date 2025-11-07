262c9ee2b75d9c2f19b73f12bbd4aae8552d5320
Софія Рехман <sofiia.rekhman@lnu.edu.ua>
2025-11-07 23:07:06 +0200

    gitignore override

--- AccommodationSystemBackend/obj/Debug/net8.0/AccommodationSystemBackend.AssemblyInfo.cs
+++ AccommodationSystemBackend/obj/Debug/net8.0/AccommodationSystemBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccommodationSystemBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ee8257f172d8589d6ac482d15f09d386dbd8f18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccommodationSystemBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccommodationSystemBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
