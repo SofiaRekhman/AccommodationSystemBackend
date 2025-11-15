a8edf4dbc46e7c5f910c3f641d319a185fdc6723
SofiaSeniv-Uni <90135240+SofiaSeniv-Uni@users.noreply.github.com>
2025-11-15 08:44:26 +0200

    Implement geting all reservations

--- BLL/Abstract/IReservationService.cs
+++ BLL/Abstract/IReservationService.cs
@@ -5,6 +5,7 @@ namespace BLL.Abstract
     public interface IReservationService
     {
         Task<int> CreateReservationAsync(PostReservationRequestModel requestModel);
+        Task<List<GetReservationsResponseModel>> GetReservationsAsync();
         Task<GetReservationResponseModel?> GetReservationByIdAsync(int reservationId);
     }
 }
